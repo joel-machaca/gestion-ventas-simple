15d6944410b60185281516337562caf234b2c7fe
Joel <joel.machaca01@gmail.com>
2025-11-28 17:42:43 -0500

    añadiendo validaciones

--- src/main/java/com/proyecto/idat/ms_gestion_ventas/dto/categoria/CategoriaRequest.java
+++ src/main/java/com/proyecto/idat/ms_gestion_ventas/dto/categoria/CategoriaRequest.java
@@ -1,5 +1,13 @@
 package com.proyecto.idat.ms_gestion_ventas.dto.categoria;
 
-public record CategoriaRequest(String nombre)
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import org.hibernate.validator.constraints.Length;
+
+public record CategoriaRequest(
+        @NotBlank
+        @Size(min = 2,max = 100, message = "el nombre debe tener entre 2 a 100 caracteres")
+        String nombre
+)
 {
 }
--- src/main/java/com/proyecto/idat/ms_gestion_ventas/dto/producto/ProductoRequest.java
+++ src/main/java/com/proyecto/idat/ms_gestion_ventas/dto/producto/ProductoRequest.java
@@ -1,9 +1,27 @@
 package com.proyecto.idat.ms_gestion_ventas.dto.producto;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
+
 public record ProductoRequest(
+        @NotBlank(message = "El nombre es obligatorio")
+        @Size(min = 2, max = 50, message = "El nombre debe tener entre 2 y 50 caracteres")
         String nombre,
+
+        @NotNull(message = "El precio de venta es obligatorio")
+        @Positive(message = "El precio debe ser positivo")
         Double precioVenta,
+
+        @NotNull(message = "El stock es obligatorio")
+        @Min(value = 0, message = "El stock mínimo es 0")
+        @Max(value = 1000, message = "El stock máximo es 1000")
         Integer stock,
+
+        @NotNull(message = "La categoría es obligatoria")
         Long idCategoria
 ) {
 }
